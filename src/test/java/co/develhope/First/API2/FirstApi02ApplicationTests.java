578d45ba687f73d20df6331339aa6dcdcfb892ca
Nicola Rossi <nicolarossi1002@gmail.com>
2022-06-23 21:46:42 +0200

    Primo commit

---
+++ src/test/java/co/develhope/First/API2/FirstApi02ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.First.API2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApi02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
